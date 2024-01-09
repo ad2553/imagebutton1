de9c4da3dd3d2511472aa865911e74b509b5e100
ad2553 <ad2553@bs.amalnet.k12.il>
2024-01-09 09:36:34 +0200

    finish

--- app/src/main/java/com/example/imagebutton1/MainActivity.java
+++ app/src/main/java/com/example/imagebutton1/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.imagebutton1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+ImageView iv;
+ImageButton im;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
+        im = findViewById(R.id.im);
+    }
+
+    public void go(View view) {
+        Random rnd = new Random();
+        int x = rnd.nextInt(3)+1;
+        if (x == 1)
+            iv.setImageResource(R.drawable.iv1);
+
+        if (x==2)
+            iv.setImageResource(R.drawable.iv2);
+
+        if (x==3)
+            iv.setImageResource(R.drawable.iv3);
+
     }
 }
